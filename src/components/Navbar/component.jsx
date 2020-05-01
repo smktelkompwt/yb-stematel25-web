1490b2d92374c9c682dda0e46b7d7264c0bb84ac
Lulu Irman Permana <irmanpermana.lu@gmail.com>
2020-05-02 06:10:18 +0700

    bugfix navbar

--- src/components/Navbar/component.jsx
+++ src/components/Navbar/component.jsx
@@ -3,6 +3,8 @@ import './style.css';
 import IMAGES from '../../config/images';
 import { NavLink as RRNavLink } from 'react-router-dom';
 import {
+  Collapse,
+  NavItem,
   NavLink
 } from 'reactstrap';
 
@@ -13,103 +15,40 @@ const Navigationbar = (props) => {
   
   return (
     <section>
-        <nav>
-          <div className="content d-flex">
-            <div className="jurusan-title-wrapper">
-              <h4>RPL25</h4>
-            </div>
-            <div className="toggle-nav-wrapper">
+      <nav>
+        <div className="content d-flex">
+          <div className="jurusan-title-wrapper">
+            <h4>RPL25</h4>
+          </div>
+          <button onClick={toggle} className="toggle-nav-wrapper">
               <img src={IMAGES.toggleNav} alt=""/>
-            </div>
-          </div>
-        </nav>
-          <div className="nav-2" >
-            <div className="container">
-                <div className="row">
-                  <div className="col">
-                    <img className="close-icon" src={IMAGES.close} alt=""/>
-                  </div>
-                </div>
-              <div className="row">
-                <div className="col">
-                    <a href="">
-                      <img className="mb-3" src={IMAGES.home} alt=""/>
-                    </a>
-                    <a href="#">
-                      <h1>Pesan dan Kesan</h1>
-                    </a>
-                    <a href="#">
-                      <h1>Daftar Guru</h1>
-                    </a>
-                    <a href="#">
-                      <h1>Daftar Siswa</h1>
-                    </a>
-                    <a href="#">
-                      <h1>Tentang Kami</h1>
-                    </a>
-                    <a href="#">
-                      <h1>Login</h1>
-                    </a>
-                </div>
-              </div>
-            </div>
-          </div>
-
+          </button>
+        </div>
+      </nav>
+      <Collapse isOpen={isOpen} navbar className="nav-2">
+        <div className="container">
+          <NavItem className="navbar-wrapper">
+            <NavLink tag={RRNavLink} exact to="/" onClick={toggle}><img className="mb-3" src={IMAGES.home} alt=""/></NavLink>
+          </NavItem>
+          <NavItem className="navbar-wrapper">
+            <NavLink className="navbar-link" tag={RRNavLink} exact to="/impression" onClick={toggle}>Pesan dan Kesan</NavLink>
+          </NavItem>
+          <NavItem className="navbar-wrapper">
+            <NavLink className="navbar-link" tag={RRNavLink} exact to="/teacher" onClick={toggle}>Daftar Guru</NavLink>
+          </NavItem>
+          <NavItem className="navbar-wrapper">
+            <NavLink className="navbar-link" tag={RRNavLink} exact to="/student" onClick={toggle}>Daftar Siswa</NavLink>
+          </NavItem>
+          <NavItem className="navbar-wrapper">
+            <NavLink className="navbar-link" tag={RRNavLink} exact to="/video" onClick={toggle}>Tentang Kami</NavLink>
+          </NavItem>
+          <NavItem className="navbar-wrapper">
+            <NavLink className="navbar-link" tag={RRNavLink} exact to="/" onClick={toggle}>Login</NavLink>
+          </NavItem>
+        </div>
+      </Collapse>
     </section>
   );
 }
 
 export default Navigationbar;
-
-// import React, { useState } from 'react';
-// import './style.css';
-// import { NavLink as RRNavLink } from 'react-router-dom';
-// import {
-//   Container,
-//   Collapse,
-//   Navbar,
-//   NavbarToggler,
-//   NavbarBrand,
-//   Nav,
-//   NavItem,
-//   NavLink,
-//   NavbarText
-// } from 'reactstrap';
-
-// const Navigationbar = (props) => {
-
-//   const [isOpen, setIsOpen] = useState(false);
-//   const toggle = () => setIsOpen(!isOpen);
-  
-//   return (
-//     <div>
-//       <Navbar className="navbar-wrapper" dark expand="md">
-//         <Container>
-//           <NavbarBrand href="/">Stematel25</NavbarBrand>
-//           <NavbarToggler onClick={toggle} />
-//           <Collapse isOpen={isOpen} navbar>
-//             <Nav className="m-auto" navbar>
-//               <NavItem>
-//                 <NavLink tag={RRNavLink} exact to="/major">Jurusan</NavLink>
-//               </NavItem>
-//               <NavItem>
-//                 <NavLink tag={RRNavLink} exact to="/impression">Kesan Pesan</NavLink>
-//               </NavItem>
-//               <NavItem>
-//                 <NavLink tag={RRNavLink} exact to="/teacher">Guru</NavLink>
-//               </NavItem>
-//               <NavItem>
-//                 <NavLink tag={RRNavLink} exact to="/video">Video</NavLink>
-//               </NavItem>
-//             </Nav>
-//             <NavbarText>Login</NavbarText>
-//           </Collapse>
-//         </Container>
-//       </Navbar>
-//     </div>
-//   );
-// }
-
-// export default Navigationbar;
-
-
