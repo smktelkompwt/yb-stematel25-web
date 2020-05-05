68dad398fd57d0b49a1c819b643df221ee2ac53d
rafiudd <sirin6867@gmail.com>
2020-05-05 13:57:55 +0700

    add: poppins font on button

--- src/components/Button/component.jsx
+++ src/components/Button/component.jsx
@@ -7,11 +7,14 @@ const Button = (props) => {
         <div className="btn-wrapper" style={{
             backgroundColor: props.bgColor ? props.bgColor : "black",
             width: props.width ? props.width : 200,
-            height: props.height ? props.height : 45,
+            height: props.height ? props.height : 45
         }}>
             <Link className="linkto" exact to={props.path} style={{
                 width: props.width ? props.width : 200,
                 height: props.height ? props.height : 45,
+                fontFamily: 'Poppins',
+                fontWeight: '600',
+                color: "#29291e"
             }}>
                 {props.text}
             </Link>
