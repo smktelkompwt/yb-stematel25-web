d78589d43a69648d5a1f6c39ec52183ea0fa3d0d
Permanaa <irmanpermana.lu@gmail.com>
2020-05-08 14:47:02 +0700

    add student detail

---
+++ src/screens/StudentDetail/component.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Button from '../../components/Button'
+import './style.css';
+
+const StudentDetail = (props) => {
+  const data = props.location.state
+  return (
+    <div className="student-detail">
+      <div style={{marginBottom: 20}}>
+        <p className="student-detail-class">{data ? data.class : 'XII RPL'}</p>/{data ? data.name : ''}
+      </div>
+      <img src={data ? data.image : require('../../assets/img/no-image.png')} alt=""/>
+      <div>
+        {biodata('Nama Lengkap', data ? data.name : '')}
+        {biodata('Kelas', data ? data.class : '')}
+        {biodata('Alamat', data ? data.address : '')}
+        {biodata('No HP', data ? data.phone : '')}
+        {biodata('Instagram', data ? data.instagram : '')}
+        {biodata('Pesan', data ? data.comment : '')}
+      </div>
+      <div className="student-detail-call">
+        <Button text='Hubungi Diaa' width="100%"/>
+      </div>
+    </div>
+  );
+}
+
+const biodata = (title, value) => {
+  return (
+    <div className="student-detail-biodata">
+      <p className="student-detail-biodata-title">{title}</p>
+      <p>{title === 'Instagram' ? `@${value}` : value}</p>
+    </div>
+  )
+}
+
+export default StudentDetail;
