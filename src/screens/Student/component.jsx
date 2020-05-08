c8d28cca4846a5461b5d2b2431e5cf5547fe2b4c
Permanaa <irmanpermana.lu@gmail.com>
2020-05-08 15:59:06 +0700

    update student

--- src/screens/Student/component.jsx
+++ src/screens/Student/component.jsx
@@ -17,34 +17,34 @@ const Student = () => {
     <div className="student">
       <h1 className="student-title">JANGAN LUPAIN<br/>DOI JUGA YA</h1>
       <div className="student-tab">
-        <Nav tabs className="student-tab-wrapper">
-          <NavItem className="student-tab-item">
+        <Nav tabs className="student-tab-wrapper row">
+          <NavItem className="student-tab-item col">
             <NavLink
-              className={classnames('student-tab-link',{ 'student-tab-active': activeTab === '1' })}
+              className={classnames('student-tab-link col',{ 'student-tab-active col': activeTab === '1' })}
               onClick={() => { toggle('1'); }}
             >
               XII RPL 1
             </NavLink>
           </NavItem>
-          <NavItem className="student-tab-item">
+          <NavItem className="student-tab-item col">
             <NavLink
-              className={classnames('student-tab-link',{ 'student-tab-active': activeTab === '2' })}
+              className={classnames('student-tab-link col',{ 'student-tab-active col': activeTab === '2' })}
               onClick={() => { toggle('2'); }}
             >
               XII RPL 2
             </NavLink>
           </NavItem>
-          <NavItem className="student-tab-item">
+          <NavItem className="student-tab-item col">
             <NavLink
-              className={classnames('student-tab-link',{ 'student-tab-active': activeTab === '3' })}
+              className={classnames('student-tab-link col',{ 'student-tab-active col': activeTab === '3' })}
               onClick={() => { toggle('3'); }}
             >
               XII RPL 3
             </NavLink>
           </NavItem>
-          <NavItem className="student-tab-item">
+          <NavItem className="student-tab-item col">
             <NavLink
-              className={classnames('student-tab-link',{ 'student-tab-active': activeTab === '4' })}
+              className={classnames('student-tab-link col',{ 'student-tab-active col': activeTab === '4' })}
               onClick={() => { toggle('4'); }}
             >
               XII RPL 4
@@ -53,7 +53,6 @@ const Student = () => {
         </Nav>
         <TabContent activeTab={activeTab} className="student-photo">
           <TabPane tabId="1">
-            <p>XII RPL 1</p>
             <div className="student-photo-list">
               {
                 students.filter(student => student.class === "XII RPL 1").map((rpl1, key) => (
@@ -63,7 +62,6 @@ const Student = () => {
             </div>
           </TabPane>
           <TabPane tabId="2">
-            <p>XII RPL 2</p>
             <div className="student-photo-list">
               {
                 students.filter(student => student.class === "XII RPL 2").map((rpl1, key) => (
@@ -73,7 +71,6 @@ const Student = () => {
             </div>
           </TabPane>
           <TabPane tabId="3">
-            <p>XII RPL 3</p>
             <div className="student-photo-list">
               {
                 students.filter(student => student.class === "XII RPL 3").map((rpl1, key) => (
@@ -83,7 +80,6 @@ const Student = () => {
             </div>
           </TabPane>
           <TabPane tabId="4">
-            <p>XII RPL 4</p>
             <div className="student-photo-list">
               {
                 students.filter(student => student.class === "XII RPL 4").map((rpl1, key) => (
