4143136a483cb7601c25805848164f3186107c3d
rafiudd <sirin6867@gmail.com>
2020-06-01 06:19:38 +0700

    feat: add tentang kami pages

--- src/screens/About/component.js
+++ src/screens/About/component.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import './style.css';
+import contributors from '../../data/contributor.json'
 
-function Video() {
+const AboutUs = () =>  {
+  const checkPath = (path) => {
+    try {
+        return require(`../../data${path}`)
+    } catch (err) {
+        return require('../../assets/img/no-image.png')
+    }
+  };
+  
   return (
-    <div className="video-container">
-      <p>Coming Soon</p>
+    <div className="container row contributor_container mx-auto">
+      {
+        contributors.map((value,key) => {
+          return(
+            <>
+            <div key={key} className="contributor__section col-lg-3 col-xl-3 col-xs-12">
+              <img className="contributor__image" src={checkPath(value.photo)} alt=""/>
+              <p className="">{value.name}</p>
+              <p className="">{value.role}</p>
+            </div>
+            </>
+          )
+        })
+      }
+      
     </div>
   );
 }
 
-export default Video;
+export default AboutUs;
